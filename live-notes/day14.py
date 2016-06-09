7f37bfa1def7148e2e015d5e78967c74d168afab
chris <asdfasd@afasdf.com>
2016-06-08 18:21:30 -0700

    removing .idea folder

--- live-notes/day14.py
+++ live-notes/day14.py
@@ -1,10 +1,11 @@
-# import requests
+import requests
 #
 # r = requests.post('https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=500&format=json')
 #
 # resp = r.json()
 # print(resp['query']['pages'][list(resp['query']['pages'].keys())[0]]['title'])
 #
+
 
 # DO NOT RUN THIS
 # num = 1
---
+++ live-notes/day15.py
@@ -0,0 +1,11 @@
+import requests
+
+# You will learn how to speak like me someday.
+headers = {"X-Mashape-Key": "fuvOMXwTedmshhs0Yt5141FbNywIp1zi6NfjsnIjUgFSOLoEam",
+           "Accept": "text/plain"}
+
+sentence = input('Enter text to be translated to Yoda speak: ')
+
+r = requests.get('https://yoda.p.mashape.com/yoda?sentence=' + sentence.replace(' ', '+'), headers=headers)
+
+print(r.text)
